490aec20641f32344df37ec9dc029862978a820b
markfour <axhfaro@gmail.com>
2018-05-31 00:19:20 +0900

    Implementation LogListTableViewCell

--- SabageLog/LogListViewController.swift
+++ SabageLog/LogListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LogListViewController.swift
 //  SabageLog
 //
 //  Created by kazumi hayashida on 2018/05/30.
@@ -8,10 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LogListViewController: UIViewController {
+  @IBOutlet private weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    tableView.register(UINib(nibName: "LogListTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
   }
 }
 
+extension LogListViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! LogListTableViewCell
+    cell.gameFiled = "TAF Zone"
+    cell.date = Date()
+    return cell
+  }
+}

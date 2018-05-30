6de149ff14c4e9de72995b6eb9fa38aad1ce3b34
markfour <axhfaro@gmail.com>
2018-05-30 22:57:48 +0900

    LogListViewModel を追加

---
+++ SabageLog/LogListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LogListViewModel.swift
+//  SabageLog
+//
+//  Created by kazumi hayashida on 2018/05/30.
+//  Copyright © 2018年 kazumi hayashida. All rights reserved.
+//
+
+import UIKit
+
+class LogListViewModel: NSObject {
+
+}

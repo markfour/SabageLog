e224135682d05b92916d875c7fb6fe9a96777ecd
markfour <axhfaro@gmail.com>
2018-07-16 22:15:48 +0900

    LogItemModelを追加

---
+++ SabageLog/LogItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  LogItemModel.swift
+//  SabageLog
+//
+//  Created by kazumi hayashida on 2018/07/16.
+//  Copyright © 2018年 kazumi hayashida. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+class LogItem {
+//  dynamic var title = ""
+}

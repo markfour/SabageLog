e59f99c198c1e76879e32657504aecafb4937918
markfour <axhfaro@gmail.com>
2018-08-05 22:07:11 +0900

    LogDetailViewControllerを追加

---
+++ SabageLog/LogDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LogDetailViewController.swift
+//  SabageLog
+//
+//  Created by kazumi hayashida on 2018/08/05.
+//  Copyright © 2018年 kazumi hayashida. All rights reserved.
+//
+
+import Foundation

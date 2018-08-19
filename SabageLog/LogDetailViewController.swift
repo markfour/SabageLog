29ae1533c353b2540a2c1eab9e53b78b77db1b6a
markfour <axhfaro@gmail.com>
2018-08-19 21:21:45 +0900

    UITableViewを追加

--- SabageLog/LogDetailViewController.swift
+++ SabageLog/LogDetailViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class LogDetailViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     navigationController!.navigationBar.topItem!.title = "戻る"
+  }
+}
+
+extension LogDetailViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! LogListTableViewCell
+//    cell.gameFiled = "TAF Zone"
+//    cell.date = Date()
+    return UITableViewCell()
   }
 }

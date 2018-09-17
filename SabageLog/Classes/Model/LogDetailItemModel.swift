8236306906cc30f664b6361708104a49fd08d7ed
markfour <axhfaro@gmail.com>
2018-09-17 19:42:00 +0900

    Add LogDetailItemModel

---
+++ SabageLog/Classes/Model/LogDetailItemModel.swift
@@ -0,0 +1,21 @@
+//
+//  LogDetailItelModel.swift
+//  SabageLog
+//
+//  Created by kazumi hayashida on 2018/09/17.
+//  Copyright © 2018年 kazumi hayashida. All rights reserved.
+//
+
+import Foundation
+
+class LogDetailItemModel {
+  enum `Type` {
+    case date
+    case potition
+    case memberNumber
+    case gameCount
+    case usedGun
+    case kill
+    case death
+  }
+}

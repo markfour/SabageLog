b0dd948108e5e104ef8eab627f5f7005ecbe1b5d
markfour <axhfaro@gmail.com>
2022-05-08 22:07:20 +0900

    SwiftUIで再作成

---
+++ SabageLog/SabageLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SabageLogApp.swift
+//  SabageLog
+//
+//  Created by kazumi hayashida on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct SabageLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

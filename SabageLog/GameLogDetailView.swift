eceac9070ebf47625238d0e80f8a8877a7d86b84
markfour <axhfaro@gmail.com>
2022-05-11 00:49:20 +0900

    GameLogDetailViewを追加した

---
+++ SabageLog/GameLogDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  GameLogDetailView.swift
+//  SabageLog
+//
+//  Created by kazumi hayashida on 2022/05/11.
+//
+
+import SwiftUI
+
+struct GameLogDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameLogDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameLogDetailView()
+    }
+}
